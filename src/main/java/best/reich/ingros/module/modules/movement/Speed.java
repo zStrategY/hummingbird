964fac59e70e62c246c94182bb2050732021e30e
CHARDNOL99 <62255498+CHARDNOL99@users.noreply.github.com>
2020-06-29 19:35:02 +0100

    made strafe work again becuz auto fucked it up
    everything is no longer chinese

--- src/main/java/best/reich/ingros/module/modules/movement/Speed.java
+++ src/main/java/best/reich/ingros/module/modules/movement/Speed.java
@@ -12,16 +12,14 @@ import me.xenforu.kelo.setting.annotation.Mode;
 import me.xenforu.kelo.setting.annotation.Setting;
 import net.b0at.api.event.Subscribe;
 import net.b0at.api.event.types.EventType;
-import net.minecraft.init.MobEffects;
 import net.minecraft.network.play.client.CPacketPlayer;
-
 import java.util.Objects;
 
 
 @ModuleManifest(label = "Speed", category = ModuleCategory.MOVEMENT, color = 0x2FBDCF)
 public class Speed extends ToggleableModule {
     @Setting("mode")
-    @Mode({"OnGround", "NCPHOP", "PACKET"})
+    @Mode({"OnGround", "Strafe", "tpSpeed"})
     public String mode = "OnGround";
     @Clamp(minimum = "0.1", maximum = "10.0")
     @Setting("Speed")
@@ -69,62 +67,6 @@ public class Speed extends ToggleableModule {
                 break;
             default:
                 break;
-        }
-    }
-
-
-    @Subscribe
-    public void onMotion(MotionEvent event) {
-        if (mc.world == null || mc.player == null) return;
-        if (mode.toUpperCase().equals("Strafe")) {
-            switch (stage) {
-                case 0:
-                    ++stage;
-                    lastDist = 0.0D;
-                    break;
-                case 2:
-                    double motionY = 0.40123128;
-                    if ((mc.player.moveForward != 0.0F || mc.player.moveStrafing != 0.0F) && mc.player.onGround) {
-                        if (mc.player.isPotionActive(MobEffects.JUMP_BOOST))
-                            motionY += ((mc.player.getActivePotionEffect(MobEffects.JUMP_BOOST).getAmplifier() + 1) * 0.1F);
-                        event.setY(mc.player.motionY = motionY);
-                        moveSpeed *= 2.149;
-                    }
-                    break;
-                case 3:
-                    moveSpeed = lastDist - (0.76 * (lastDist - getBaseMoveSpeed()));
-                    break;
-                default:
-                    if ((mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0.0D, mc.player.motionY, 0.0D)).size() > 0 || mc.player.isCollidedVertically) && stage > 0) {
-                        stage = mc.player.moveForward == 0.0F && mc.player.moveStrafing == 0.0F ? 0 : 1;
-                    }
-                    moveSpeed = lastDist - lastDist / 159.0D;
-                    break;
-            }
-            moveSpeed = Math.max(moveSpeed, getBaseMoveSpeed());
-            double forward = mc.player.movementInput.moveForward, strafe = mc.player.movementInput.moveStrafe, yaw = mc.player.rotationYaw;
-            if (forward == 0.0F && strafe == 0.0F) {
-                event.setX(0);
-                event.setZ(0);
-            }
-            if (forward != 0 && strafe != 0) {
-                forward = forward * Math.sin(Math.PI / 4);
-                strafe = strafe * Math.cos(Math.PI / 4);
-            }
-            event.setX((forward * moveSpeed * -Math.sin(Math.toRadians(yaw)) + strafe * moveSpeed * Math.cos(Math.toRadians(yaw))) * 0.99D);
-            event.setZ((forward * moveSpeed * Math.cos(Math.toRadians(yaw)) - strafe * moveSpeed * -Math.sin(Math.toRadians(yaw))) * 0.99D);
-            ++stage;
-        }
-    }
-
-    private double getBaseMoveSpeed() {
-        double baseSpeed = 0.272;
-        if (mc.player.isPotionActive(MobEffects.SPEED)) {
-            final int amplifier = Objects.requireNonNull(mc.player.getActivePotionEffect(MobEffects.SPEED)).getAmplifier();
-            baseSpeed *= 1.0 + (0.2 * amplifier);
-        }
-        return baseSpeed;
-    }
 
     private double[] getDirectionalSpeed(final double speed) {
         float forward = mc.player.movementInput.moveForward;
