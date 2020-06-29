033bdede0dc1b917b7683f28afc11f1c001047b3
CHARDNOL99 <62255498+CHARDNOL99@users.noreply.github.com>
2020-06-29 15:47:10 +0100

    Update and rename WebHoleFill.java to CumInDaHole.java

--- src/main/java/best/reich/ingros/module/modules/combat/CumInDaHole.java
+++ src/main/java/best/reich/ingros/module/modules/combat/CumInDaHole.java
@@ -26,7 +26,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @ModuleManifest(label = "CumInDaHole", category = ModuleCategory.COMBAT, color = 0xffAEffAE)
-public class HoleFiller extends ToggleableModule {
+public class CumInDaHole extends ToggleableModule {
     @Setting("Fade")
     public boolean fade = true;
     @Setting("Radius")
