ceb208b30c400d6a6ae65ecb7f9004371f2b55ce
CHARDNOL99 <62255498+CHARDNOL99@users.noreply.github.com>
2020-07-07 13:07:19 +0100

    finally i can peeveepee like my idol Zb0b

---
+++ src/main/java/best/reich/ingros/module/modules/render/ViewModelChanger.java
@@ -0,0 +1,17 @@
+package best.reich.ingros.module.modules.render;
+
+import me.xenforu.kelo.module.ModuleCategory;
+import me.xenforu.kelo.module.annotation.ModuleManifest;
+import me.xenforu.kelo.module.type.ToggleableModule;
+import net.minecraft.item.ItemSword;
+
+@ModuleManifest(label = "ViewModelChanger", category = ModuleCategory.RENDER, color = 0xffffff33)
+public class ViewModelChanger extends ToggleableModule {
+
+    public void onUpdate(){
+        if(mc.player.getHeldItemMainhand().getItem() instanceof ItemSword && mc.entityRenderer.itemRenderer.prevEquippedProgressMainHand >= 0.9) {
+            mc.entityRenderer.itemRenderer.equippedProgressMainHand = 1;
+            mc.entityRenderer.itemRenderer.itemStackMainHand = mc.player.getHeldItemMainhand();
+        }
+    }
+}
